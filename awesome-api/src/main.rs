2c9c1e267da62659c54c927538e2bf4b1b5ced2b
Yuki Kodama <endflow.net@gmail.com>
2023-01-25 21:41:09 +0900

    First commit

---
+++ awesome-api/src/main.rs
@@ -0,0 +1,17 @@
+use std::net::SocketAddr;
+
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(root));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
+}
